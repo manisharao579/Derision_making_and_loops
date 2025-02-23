065e69ddad86082661047a6221e877c2df9698c7
Manisha Yadav <manisha.rao579@gmail.com>
2025-02-23 12:26:16 +0530

    Add files via upload

---
+++ ForEachLoop.java
@@ -0,0 +1,12 @@
+package ControlStatements;
+
+public class ForEachLoop
+{
+    public static void main(String[] args) {
+        int arr[] = {9,7,90};
+
+        for(int a : arr){
+            System.out.println(a);
+        }
+    }
+}
