b06c32cfc9b3b425b02a2b7b4a2861c19f78f8a2
Manisha Yadav <manisha.rao579@gmail.com>
2025-02-23 13:26:32 +0530

    Add files via upload

---
+++ continueStatement.java
@@ -0,0 +1,12 @@
+package ControlStatements;
+
+public class continueStatement {
+    public static void main(String [] args) {
+        for(int i=1 ; i<=10 ; i++){
+            if(i==5){
+                continue ;
+            }
+            System.out.println(i);
+        }
+    }
+}
